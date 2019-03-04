92085b1d0e3e63d7390204c39be3848495a54edd
Ryan Pavlick <rpavlick@users.noreply.github.com>
2019-03-03 22:43:42 -0800

    Moved example to setup_dock.sh

--- add_to_dock.sh
+++ add_to_dock.sh
@@ -13,19 +13,3 @@ function add_spacer_to_dock {
 function clear_dock {
     defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="small-spacer-tile";}'
 }
-
-# WARNING: clears existing dock
-clear_dock
-add_app_to_dock "System Preferences"
-add_spacer_to_dock
-add_app_to_dock "Microsoft Outlook"
-add_app_to_dock "Google Chrome"
-add_app_to_dock "Microsoft Excel"
-add_app_to_dock "Microsoft Word"
-add_app_to_dock "Microsoft Powerpoint"
-add_app_to_dock "kitty"
-add_app_to_dock "Visual Studio Code"
-add_app_to_dock "SelfControl"
-add_app_to_dock "Skype"
-
-killall Dock
