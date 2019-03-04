487476824f3ff134c1137edb51bd2acbe481abff
rpavlick <ryan.pavlick@gmail.com>
2019-03-04 13:30:06 -0800

    fixed some bugs

--- add_to_dock.sh
+++ add_to_dock.sh
@@ -1,25 +1,30 @@
-function find_app {
-    app_name=$1
-    launchservices_path="/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister"
-    ${launchservices_path} -dump | grep -o "/.*${app_name}.app" | grep -v -E "Backups|Caches|TimeMachine|Temporary|/Volumes/${app_name}" | uniq | sort | head -n1
-}
-
+# adds an application to Dock
+# usage: add_app_to_dock "Application Name" 
+# example add_app_to_dock "Terminal"
 function add_app_to_dock {
-    app_path=$(find_app "${1}")
+    app_name="${1}"
+    launchservices_path="/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister"
+    app_path=$(${launchservices_path} -dump | grep -o "/.*${app_name}.app" | grep -v -E "Backups|Caches|TimeMachine|Temporary|/Volumes/${app_name}" | uniq | sort | head -n1)
     if open -Ra "${app_path}"; then
         echo "$app_path added to the Dock."
         defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>${app_path}</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
     else
-        echo "ERROR: $app_path not found." 1>&2
-        exit 1 # terminate and indicate error
+        echo "ERROR: $1 not found." 1>&2
     fi
 }
 
+# adds an empty space to Dock
 function add_spacer_to_dock {
     defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="small-spacer-tile";}'
 }
 
+# removes all persistent icons from Dock
 function clear_dock {
-    defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="small-spacer-tile";}'
+    defaults write com.apple.dock persistent-apps -array
+}
+
+# reset Dock to default settings
+function reset_dock {
+    defaults write com.apple.dock; killall Dock
 }
 
--- setup_dock.sh
+++ setup_dock.sh
@@ -10,10 +10,11 @@ add_app_to_dock "Microsoft Outlook"
 add_app_to_dock "Google Chrome"
 add_app_to_dock "Microsoft Excel"
 add_app_to_dock "Microsoft Word"
-add_app_to_dock "Microsoft Powerpoint"
+add_app_to_dock "Microsoft PowerPoint"
 add_app_to_dock "kitty"
 add_app_to_dock "Visual Studio Code"
 add_app_to_dock "SelfControl"
 add_app_to_dock "Skype"
+add_spacer_to_dock
 
 killall Dock
