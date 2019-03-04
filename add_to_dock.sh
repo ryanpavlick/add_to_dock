16a0b910e12c73d49632b465992605aa1ecfb10c
Ryan Pavlick <rpavlick@users.noreply.github.com>
2019-03-04 13:06:40 -0800

    Update add_to_dock.sh

--- add_to_dock.sh
+++ add_to_dock.sh
@@ -1,8 +1,17 @@
+function find_app {
+    app_name=$1
+    launchservices_path="/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister"
+    ${launchservices_path} -dump | grep -o "/.*${app_name}.app" | grep -v -E "Backups|Caches|TimeMachine|Temporary|/Volumes/${app_name}" | uniq | sort | head -n1
+}
+
 function add_app_to_dock {
-    app_path="/Applications/$(echo ${1// /\ }).app"
+    app_path=$(find_app "${1}")
     if open -Ra "${app_path}"; then
-        echo "$app_path"
+        echo "$app_path added to the Dock."
         defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>${app_path}</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
+    else
+        echo "ERROR: $app_path not found." 1>&2
+        exit 1 # terminate and indicate error
     fi
 }
 
@@ -13,3 +22,4 @@ function add_spacer_to_dock {
 function clear_dock {
     defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="small-spacer-tile";}'
 }
+
