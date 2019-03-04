1c5b22b2f2669f9405f08f90d9b566e874623d9b
Ryan Pavlick <rpavlick@users.noreply.github.com>
2019-03-03 22:42:47 -0800

    Create setup_dock.sh

---
+++ setup_dock.sh
@@ -0,0 +1,18 @@
+source ./add_to_dock.sh
+
+# WARNING: clears existing dock
+clear_dock
+
+add_app_to_dock "System Preferences"
+add_spacer_to_dock
+add_app_to_dock "Microsoft Outlook"
+add_app_to_dock "Google Chrome"
+add_app_to_dock "Microsoft Excel"
+add_app_to_dock "Microsoft Word"
+add_app_to_dock "Microsoft Powerpoint"
+add_app_to_dock "kitty"
+add_app_to_dock "Visual Studio Code"
+add_app_to_dock "SelfControl"
+add_app_to_dock "Skype"
+
+killall Dock
